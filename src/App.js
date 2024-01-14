1fafcddcfba16b7fc0a0eeae3bb4397d94e58305
Yash Balkhande <78372068+balkhandeyash@users.noreply.github.com>
2024-01-14 23:58:46 +0530

    First Commit, Updation in the files and inclusion of .env file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Weather from "./components/weather";
+import { Dimmer, Loader } from "semantic-ui-react";
+export default function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
-function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(async function (position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        try {
+          const response = await fetch(
+            `${process.env.REACT_APP_API_URL}/weather/?lat=${latitude}&lon=${longitude}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+          );
+
+          if (!response.ok) {
+            throw new Error("Failed to fetch weather data");
+          }
+
+          const result = await response.json();
+          setData(result);
+          console.log(result);
+        } catch (error) {
+          console.error("Error fetching weather data:", error);
+        }
+      });
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {typeof data.main != "undefined" ? (
+        <Weather weatherData={data} />
+      ) : (
+        <div>
+          <Dimmer active>
+            <Loader>Loading..</Loader>
+          </Dimmer>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
